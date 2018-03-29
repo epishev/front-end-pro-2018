1960a370c9dd359be732b6c086b5695ba44998a4
Eugen <eepishev1989@gmail.com>
2018-03-29 10:18:54 +0300

    Homework3 changes

--- HomeWork3/scripts/task1_4_rhombus.js
+++ HomeWork3/scripts/task1_4_rhombus.js
@@ -1,21 +1,11 @@
 const separator = '*';
 const space = '&nbsp;';
 const next  = '<br />';
-var a = 51;
-var b = Math.round(a / 2 - 1);
-var c = Math.round(a / 2 + 1);
+var a = 50;
+var b = Math.round(a / 2);
+var c = Math.round(a / 2);
 
-
-for (var i = 1; i <= a; i++) {
-    if ( i === Math.round(a / 2)) {
-        document.write(separator);
-    } else {
-        document.write(space);
-    }
-}
-document.write(next);
-
-for (var i = 1; i <= Math.round(a / 2 - 1); i++) {
+for (var i = 1; i <= Math.round(a / 2); i++) {
     for (var j = 1; j <= a; j++) {
         if (j == b || j == c) {
             document.write(separator);
@@ -31,7 +21,7 @@ for (var i = 1; i <= Math.round(a / 2 - 1); i++) {
 b = 2;
 c = (a % 2 === 0) ? a - 2 : a - 1;
 
-for (var i = 1; i <= Math.round(a / 2 - 2); i++) {
+for (var i = 1; i <= Math.round(a / 2 - 1); i++) {
     for (var j = 1; j <= a; j++) {
         if (j == b || j == c) {
             document.write(separator);
@@ -42,14 +32,6 @@ for (var i = 1; i <= Math.round(a / 2 - 2); i++) {
     document.write(next);
     b++;
     c--;
-}
-
-for (var i = 1; i <= a; i++) {
-    if ( i === Math.round(a / 2)) {
-        document.write(separator);
-    } else {
-        document.write(space);
-    }
 }
 
 
